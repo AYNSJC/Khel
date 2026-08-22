e9f4c5c46a6f681f3a8cf2d507bc9afa54d5323e
AYNSJC <148061599+AYNSJC@users.noreply.github.com>
2026-08-22 10:35:35 +0530

    New dll

--- KhelEngine/obj/Release/net8.0/KhelEngine.AssemblyInfo.cs
+++ KhelEngine/obj/Release/net8.0/KhelEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KhelEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f564b2ac5ca79eef0c60a68f1f7b204fffe4bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b3f0c86b76ac23f8af77e5542240f343df66ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("KhelEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KhelEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
